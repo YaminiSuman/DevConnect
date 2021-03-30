8de92f3c678f821a3534c9ac1429ad5a9a41f6fc
YaminiSuman <me.yamini.01@gmail.com>
2021-03-30 15:18:18 +0530

    User registration api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 // @route   POST api/users
@@ -17,13 +19,37 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
-    console.log(req.body);
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+    try {
+      //if user exist return 400
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists..' }] });
+      }
+
+      //get gravator
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+      user = new User({ name, email, password, avatar });
+      //encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send('User route');
+      //return json-web-token
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
