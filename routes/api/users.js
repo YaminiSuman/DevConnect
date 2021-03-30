d68d3fff1c7ff19c97596b8947cae319694d4491
YaminiSuman <me.yamini.01@gmail.com>
2021-03-30 15:08:26 +0530

    User body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
-// @route   GET api/users
-// @desc    Test route
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include valid email').isEmail(),
+    check(
+      'password',
+      'Please enter password with 6 or more characters'
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 //connect db
 connectDB();
 
+//Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 //Define Routes
