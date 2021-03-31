908890d55666d1866c88d2cb27f5b7f7a742b72c
YaminiSuman <me.yamini.01@gmail.com>
2021-03-31 15:24:50 +0530

    JWT validation and protected route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Protected route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
-
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 module.exports = router;
