f9741936bdf7ab1d850eb6f0f0eb5ca2703239fd
YaminiSuman <me.yamini.01@gmail.com>
2021-04-12 01:45:12 +0530

    Use axios to register user

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,101 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Fragment, useState } from 'react';
 
 function Register() {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const { name, email, password, confirmPassword } = formData;
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const submitHandle = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      console.log('Password do not match');
+    } else {
+      const newUser = { name, email, password };
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
+
+        const body = JSON.stringify(newUser);
+        const res = await axios.post('/api/users', body, config);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    }
+  };
+  return (
+    <Fragment>
+      {' '}
+      <section className='container'>
+        <h1 className='large text-primary'>Sign Up</h1>
+        <p className='lead'>
+          <i className='fas fa-user'></i> Create Your Account
+        </p>
+        <form className='form' onSubmit={submitHandle}>
+          <div className='form-group'>
+            <input
+              type='text'
+              placeholder='Name'
+              name='name'
+              value={name}
+              onChange={changeHandler}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={changeHandler}
+            />
+            <small className='form-text'>
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              minLength='6'
+              value={password}
+              onChange={changeHandler}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Confirm Password'
+              name='confirmPassword'
+              minLength='6'
+              value={confirmPassword}
+              onChange={changeHandler}
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Register' />
+        </form>
+        <p className='my-1'>
+          Already have an account? <a href='login.html'>Sign In</a>
+        </p>
+      </section>
+    </Fragment>
+  );
 }
 
 export default Register;
