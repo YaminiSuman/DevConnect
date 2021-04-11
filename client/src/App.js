48c1fb68e896ed1a0efb5b326af6641507bc718a
YaminiSuman <me.yamini.01@gmail.com>
2021-04-12 00:17:06 +0530

    React router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
-
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
