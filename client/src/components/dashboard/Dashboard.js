b9c2c100f2e36f42d095f4b74f5c0a23550aee27
YaminiSuman <me.yamini.01@gmail.com>
2021-04-25 13:13:04 +0530

    Display Education and Experience in Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import { Link } from 'react-router-dom';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 function Dashboard({
   getCurrentProfile,
@@ -24,6 +26,8 @@ function Dashboard({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
