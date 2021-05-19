6dd9bcf887cdae656068ed55e2303e35da6ebb94
YaminiSuman <me.yamini.01@gmail.com>
2021-05-19 22:27:27 +0530

    Changes to push build in heroku

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 //connect db
 connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('API running'));
 
 //Define Routes
 app.use('/api/sample', require('./routes/api/sample/sampleApi'));
@@ -17,5 +15,14 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
